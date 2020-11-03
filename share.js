dd25d417df3015f8b230ee45d6e215a2f3e2c480
LucasZuk <lucas.zukervar@gmail.com>
2020-11-03 08:09:44 -0300

    Add tablet mode

--- share.js
+++ share.js
@@ -1,4 +1,4 @@
-const content = '<div id="content"><ul id="share" class="article_preview_bottom_profil_share">SHARE<li id="facebook"><a href="http://www.facebook.com" target="_blank"><img src="./images/icon-facebook.svg" alt="Facebook"></a></li><li id="twitter"><a href="http://www.twitter.com" target="_blank"><img src="./images/icon-twitter.svg" alt="twitter"></a></li><li id="pinterest"><a href="http://www.pinterest.com" target="_blank"><img src="./images/icon-pinterest.svg" alt="pinterest"></a></li></ul></div>'
+const content = '<div id="content"><ul id="share" class="article_preview_bottom_profil_share">SHARE<li id="facebook"><a href="http://www.facebook.com" target="_blank"><img id="top1" src="./images/icon-facebook.svg" alt="Facebook"/></a></li><li id="twitter"><a href="http://www.twitter.com" target="_blank"><img src="./images/icon-twitter.svg" alt="twitter"></a></li><li id="pinterest"><a href="http://www.pinterest.com" target="_blank"><img src="./images/icon-pinterest.svg" alt="pinterest"></a></li></ul></div>'
 
 tippy('#share', {
   content: content,
@@ -6,5 +6,5 @@ tippy('#share', {
   theme : "sharingBox",
   interactive : true,
   offset: [0, 25],
+  interactiveBorder : 10,
 });
-
